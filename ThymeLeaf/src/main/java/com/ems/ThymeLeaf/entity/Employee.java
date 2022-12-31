1fc633c1add0514cf8bbea22c52c8a4a8d897080
sovan2001 <contact.sovan2001@gmail.com>
2022-12-31 09:12:52 +0530

    Added Cascading Type in Eager In Employee.java

--- ThymeLeaf/src/main/java/com/ems/ThymeLeaf/entity/Employee.java
+++ ThymeLeaf/src/main/java/com/ems/ThymeLeaf/entity/Employee.java
@@ -1,7 +1,9 @@
 package com.ems.ThymeLeaf.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,7 +16,7 @@ import jakarta.persistence.UniqueConstraint;
 public class Employee {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int empId;
 	@Column
 	private String empUsrnm;
@@ -22,7 +24,7 @@ public class Employee {
 	private String empPwd;
 	@Column
 	private String empRole;
-	@OneToOne(mappedBy = "employee")
+	@OneToOne(mappedBy = "employee",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
     private User user;
 	
 	public Employee() {
